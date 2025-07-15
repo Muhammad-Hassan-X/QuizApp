4a98de549a313efad3fc09fea612181933e8ae70
Muhammad Hassan <muhammadhassan.developer>
2025-07-15 15:48:25 +0500

    Res / images.ts added

---
+++ res/images.ts
@@ -0,0 +1,6 @@
+const images = {
+	splash: require("@/assets/images/splash.png"),
+	splash2: require("@/assets/images/splash2.png"),
+	splash3: require("@/assets/images/splash3.png"),
+};
+export default images;
